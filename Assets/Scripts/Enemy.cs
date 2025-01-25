efbda60deff53fd07f9cc0d3de4d4d60daadf227
pham-t-alex <pham.t.alex@gmail.com>
2025-01-24 23:42:34 -0800

    player center as hitbox overhaul

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -25,13 +25,17 @@ public class Enemy : MonoBehaviour
 
     void CheckIfPlayerInView()
     {
-        Vector2 playerPosition = Vector2.zero; // REPLACE WHEN DONE WITH PLAYER
-        if (Vector2.Distance(transform.position, playerPosition) > range)
+        // VER 2: PLAYER HITBOX IS JUST CENTER
+        Vector2 viewCenterPos = (Vector2)transform.position + viewCenter;
+        Vector2 playerPosition = Vector2.zero; // REPLACE WHEN DONE WITH PLAYER COLLIDER CENTER
+        if (Vector2.Distance(viewCenterPos, playerPosition) > range)
         {
             Debug.Log("too far");
             return;
         }
-        float angle = Vector2.Angle(Quaternion.Euler(0, 0, viewAngleCenter) * Vector2.right, playerPosition - (Vector2)transform.position);
+        float angle = Vector2.Angle(Quaternion.Euler(0, 0, viewAngleCenter) * Vector2.right, playerPosition - viewCenterPos);
+        // testing edges of view
+        
         if (angle > angleWidth / 2)
         {
             Debug.Log("outside of view");
@@ -39,22 +43,78 @@ public class Enemy : MonoBehaviour
         }
         int layerMask = 1 << 3;
         layerMask |= 1 << 6;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, playerPosition - (Vector2)transform.position, range, layerMask);
+        RaycastHit2D hit = Physics2D.Raycast(viewCenterPos, playerPosition - (Vector2)viewCenterPos, range, layerMask);
         if (hit)
         {
             if (hit.collider.gameObject.name == "Player") // REPLACE WITH .GetComponent<Player>()
             {
-                Debug.Log("PLAYER IN VIEW");
+                Debug.Log("PLAYER IN VIEW 3");
                 // DO SOMETHING
-            }
-            else
-            {
-                Debug.Log("obstructed");
+                return;
             }
         }
+
         Debug.Log("no hit");
 
-        // devise alternative solution using raycasts and the player's body
-        // also center it at viewCenter + transform position instead of transform position
+        // VER 1: PRECISE PLAYER HITBOX
+        /*Vector2 viewCenterPos = (Vector2)transform.position + viewCenter;
+        Vector2 playerPosition = Vector2.zero; // REPLACE WHEN DONE WITH PLAYER COLLIDER CENTER
+        float playerDiagonalWidth = 1.4f / 2; // REPLACE WITH PLAYER DIAGONAL WIDTH (sqrt BOUNDS.EXTENTS^2)
+        if (Vector2.Distance(viewCenterPos, playerPosition) > range + playerDiagonalWidth)
+        {
+            Debug.Log("too far");
+            return;
+        }
+        float angle = Vector2.Angle(Quaternion.Euler(0, 0, viewAngleCenter) * Vector2.right, playerPosition - viewCenterPos);
+        // testing edges of view
+        int layerMask = 1 << 3;
+        layerMask |= 1 << 6;
+
+        // check edges of angle bounding 
+        RaycastHit2D hit1 = Physics2D.Raycast(viewCenterPos, Quaternion.Euler(0, 0, viewAngleCenter - (angleWidth / 2)) * Vector2.right, range, layerMask);
+        if (hit1 && hit1.collider.gameObject.name == "Player") // REPLACE WITH .GetComponent<Player>()
+        {
+            Debug.Log("PLAYER IN VIEW 1");
+            return;
+            // DO SOMETHING
+        }
+        RaycastHit2D hit2 = Physics2D.Raycast(viewCenterPos, Quaternion.Euler(0, 0, viewAngleCenter + (angleWidth / 2)) * Vector2.right, range, layerMask);
+        if (hit2 && hit2.collider.gameObject.name == "Player") // REPLACE WITH .GetComponent<Player>()
+        {
+            Debug.Log("PLAYER IN VIEW 2");
+            return;
+            // DO SOMETHING
+        }
+        if (angle > angleWidth / 2)
+        {
+            Debug.Log("outside of view");
+            return;
+        }
+
+        Vector2[] playerPoints = new Vector2[5];
+        float xExtents = 0.5f; // REPLACE WITH PLAYER COLLIDER EXTENTS
+        float yExtents = 0.5f; // REPLACE WITH PLAYER COLLIDER EXTENTS
+        playerPoints[0] = playerPosition;
+        playerPoints[1] = playerPosition + new Vector2(-xExtents, -yExtents);
+        playerPoints[2] = playerPosition + new Vector2(-xExtents, yExtents);
+        playerPoints[3] = playerPosition + new Vector2(xExtents, -yExtents);
+        playerPoints[4] = playerPosition + new Vector2(xExtents, yExtents);
+        for (int i = 0; i < 4; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(viewCenterPos, playerPoints[i] - (Vector2)viewCenterPos, range, layerMask);
+            if (hit)
+            {
+                if (hit.collider.gameObject.name == "Player") // REPLACE WITH .GetComponent<Player>()
+                {
+                    Debug.Log("PLAYER IN VIEW 3");
+                    // DO SOMETHING
+                    return;
+                }
+            }
+        }
+
+        Debug.Log("no hit");
+        */
+
     }
 }
